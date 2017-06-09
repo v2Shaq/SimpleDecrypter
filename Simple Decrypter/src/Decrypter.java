97dc18fc7e76e65f1e7ff521d6deff711fbc5091
th30r3tisch <theoretisch@hotmail.de>
2017-06-09 02:23:41 +0200

    add common words from www.ef.com and added decryption methods for the shortest words

--- Simple Decrypter/src/Decrypter.java
+++ Simple Decrypter/src/Decrypter.java
@@ -6,59 +6,143 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * @author Theor
+ *
+ */
 
 public class decrypter {
 
-	private String line;
 	private String text;
-	ArrayList<Character> letters = new ArrayList<>();
-	ArrayList<Integer> counts = new ArrayList<>();
-	HashMap<Integer, Character> map = new LinkedHashMap<Integer, Character>();
-	String cleanString;
-	ArrayList<String> sortedWordlist;
+	private String cleanString;
+	private String filepath;
+	
+	private ArrayList<Character> letters;
+	private ArrayList<Integer> counts;
+	private ArrayList<String> sortedWordlist;
+	
+	private HashMap<Integer, Character> map;
+	private static Set<String> dict;
+	private static Set<String> textset;
+	
 	
 	public decrypter(){
+		letters = new ArrayList<>();
+		counts = new ArrayList<>();
+		map = new LinkedHashMap<Integer, Character>();
 
+		filepath = "C:/Users/Theor/Desktop/Eclipse/workspace/itsicherheit/";
 	}
 	
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 		decrypter dc = new decrypter();
 		try {
-			dc.readFile();
-			dc.decrypt();
+			textset = dc.readFile( "sample.txt_enc");
+			dict = dc.readFile( "3kMonstCommonWords.txt");
 		} catch (FileNotFoundException e) {
 			System.out.println("there is no file to read");
 			e.printStackTrace();
 		}
+		dc.decrypt();
 	}
 
-	private void readFile() throws FileNotFoundException {
-		Scanner fileIn = new Scanner(new File("C:/Users/Theor/Desktop/Eclipse/workspace/itsicherheit/sample.txt_enc"));
+	
+	/**
+	 * reads the file
+	 * @param filename	name of the file
+	 * @param output	set of lines of the file
+	 * @throws FileNotFoundException
+	 */
+	private Set<String> readFile(String filename) throws FileNotFoundException {
+		Set<String> output = new HashSet<>();
+		Scanner fileIn = new Scanner(new File(filepath + filename));
 		while (fileIn.hasNextLine()) {
-			line = fileIn.nextLine();
+			output.add(fileIn.nextLine());
+		}	
+		fileIn.close();
+		return output;
+	}
+	
+	
+	/**
+	 * concats the set of encrypted lines to one string
+	 */
+	private void maketoString(){
+		for (String line : textset) {
 			if (text == null) {
 				text = line;
 			}else{
 				text = text.concat(line);
 			}
-		}	
-		fileIn.close();
+		}
 	}
 	
+	
+	/**
+	 * decrypts the string
+	 */
 	private void decrypt(){
+		maketoString();
 		countLetters();
 		printLetterStats();
 		makeWords();
-		cleanString = cleanString.replace("f", "e"); // most used char is e
-		//cleanString = cleanString.replaceAll("(?<=t).?", "h"); // first char after t --> most often syllable th
-		//cleanString = cleanString.replaceAll(".?(?=e)", "h"); // first char before e --> second most often syllable he TODO inserts not replaces -> wrong
-//		cleanString = cleanString.replace("v", "i"); // single chars probably are I or a
-//		cleanString = cleanString.replace("t", "a"); // single chars probably are I or a
+		cleanString = cleanString.replace( letters.get(1), 'e'); // most used char is e
+		decOneLetterWords();
+		decTwoLetterWords();
 		// d seems to be the same char (d) --> idea
 	}
 	
+	/**
+	 * decrypts words of length one
+	 */
+	private void decOneLetterWords(){
+		ArrayList<String> words1 = new ArrayList<>();
+		words1 = getWordsOfSameLength(1);
+		cleanString = cleanString.replace("v", "i"); // single chars probably are I or a
+		cleanString = cleanString.replace("t", "a"); // single chars probably are I or a
+	}
+	
+	/**
+	 * decrypts words of length two
+	 */
+	private void decTwoLetterWords(){
+		ArrayList<String> words2 = new ArrayList<>();
+		getWordsOfSameLength(2);
+	}
+	
+	/**
+	 * decrypts words of length three
+	 */
+	private void decThreeLetterWords(){
+		ArrayList<String> words3 = new ArrayList<>();
+		getWordsOfSameLength(3);
+		//cleanString = cleanString.replaceAll("(?<=t).?", "h"); // first char after t --> most often syllable th
+		//cleanString = cleanString.replaceAll(".?(?=e)", "h"); // first char before e --> second most often syllable he TODO inserts not replaces -> wrong
+	}
+	
+	/**
+	 * @param wordlength	length of the searched word
+	 * @return				list of all words with length of wordlength
+	 */
+	private ArrayList<String> getWordsOfSameLength(int wordlength){
+		ArrayList<String> words = new ArrayList<>();
+		for (String word : sortedWordlist) {
+			if(word.length() != wordlength){
+				break;
+			}
+			words.add(word);
+		}
+		return words;
+	}
+	
+	/**
+	 * counts the number of each letter in the encrypted string
+	 */
 	private void countLetters(){
 		int i = 0;
 		while (text.length() > i) {
@@ -75,9 +159,13 @@ public class decrypter {
 		}
 	}
 	
+	
+	/**
+	 * splits the encrypted string into words
+	 */
 	private void makeWords(){
-		char test = map.get(counts.get(0)); // most existing char
-		cleanString = text.replace(test, ' '); // see words
+		char mostChar = map.get(counts.get(0)); // most existing char
+		cleanString = text.replace(mostChar, ' '); // see words
 		String[] words = cleanString.split(" ");
 		ArrayList<String> wordlist = new ArrayList<>(Arrays.asList(words));
 		sortedWordlist = new ArrayList<>();
@@ -97,6 +185,9 @@ public class decrypter {
 	}
 	
 	
+	/**
+	 * prints the probablility of each letter
+	 */
 	private void printLetterStats(){
 		int i = 0;
 		while(letters.size() > i){
